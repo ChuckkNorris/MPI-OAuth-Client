b28293cfb53a7faea296b81ddb84081d2ca8b9c9
Levi <lfuller941@gmail.com>
2017-07-17 16:36:27 -0600

    Init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Http } from "@angular/http";
+import * as Oidc from 'oidc-client';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  constructor(
+    private _http: Http
+  ) {
+    
+  }
+  
+
+  login() {
+    var config = {
+      authority: "http://localhost:5000",
+      client_id: "js",
+      redirect_uri: "http://localhost:4200",
+      response_type: "id_token token",
+      scope:"openid profile api1",
+      post_logout_redirect_uri : "http://localhost:4200",
+    };
+    let mgr = new Oidc.UserManager(config);
+    console.log(Oidc);
+    mgr.signinRedirect();
+    //this._http.
+  }
 }
